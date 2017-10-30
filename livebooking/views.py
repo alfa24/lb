2ef776112482c75174961d3a0207188b8792ade0
Falichev Aleksandr <6064233@gmail.com>
2017-10-30 17:11:29 +0800

    fix search page

--- livebooking/views.py
+++ livebooking/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-import requests
-from livebooking import settings
 
 
 def index(request):
-    search_types = settings.search_types
     return render(request, 'main/index.html', locals())
--- services/views.py
+++ services/views.py
@@ -1,24 +1,15 @@
 from django.shortcuts import render
 # import requests
 from users.models import ProfileCompany
-from services.models import Service
-from livebooking import settings
 
 
 def search_result(request):
-    setting = settings
-    search_type = setting.PROFESSIONALS
-    search_types = settings.search_types
     search_text = ''
 
     if request.GET:
-        type_search = int(request.GET['search_type'])
         search_text = request.GET['search_text']
 
-    if type_search == settings.PROFESSIONALS:
-        items = ProfileCompany.objects.all()
-    elif type_search == settings.SERVICES:
-        items = Service.objects.all()
+    items = ProfileCompany.objects.all()
 
     return render(request, 'services/search_results.html', locals())
 
